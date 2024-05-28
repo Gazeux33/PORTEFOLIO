ba88f414d229ff5e759605b4acefa4968fce7d2b
Gazeux33-IUT <theocastillo@yahoo.com>
2024-05-28 09:01:41 +0200

    up

--- src/Composants/ContactMe/ContactMe.js
+++ src/Composants/ContactMe/ContactMe.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React from "react";
 import "./ContactMe.css";
 
-
 function ContactMe() {
-
-    return (
-        <div id="contact-me-container">
-            <h1>Contact Me</h1>
-            <div id="contacts-container">
-                <p>Email: theocastillo@yahoo.com</p>
-                <p>Telephone : 0616183892</p>
+  return (
+    <div id="contact-me-container">
+      <h1>Contact Me</h1>
+      <div id="contacts-container">
+        <div className="form-container">
+          <form className="form">
+            <div className="form-group">
+              <label for="email">Company Email</label>
+              <input required="" name="email" id="email" type="text" />
             </div>
+            <div className="form-group">
+              <label for="textarea">How Can We Help You?</label>
+              <textarea
+                required=""
+                cols="50"
+                rows="10"
+                id="textarea"
+                name="textarea"
+              >
+                {" "}
+              </textarea>
+            </div>
+            <button type="submit" className="form-submit-btn">
+              Submit
+            </button>
+          </form>
         </div>
-
-    );
+      </div>
+    </div>
+  );
 }
 
 export default ContactMe;
