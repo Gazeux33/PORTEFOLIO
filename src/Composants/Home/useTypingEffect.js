238334b3fc250b48e413ccb7c2a0da7de58724e2
Theo <thoecastillo@yahoo.com>
2024-05-30 17:03:49 +0200

    up

--- src/Composants/Home/useTypingEffect.js
+++ src/Composants/Home/useTypingEffect.js
@@ -4,16 +4,6 @@ const useTypingEffect = (wordElement, words, typingDelay = 100, erasingDelay = 1
     const wordIndex = useRef(0);
     const charIndex = useRef(0);
 
-    const type = useCallback(() => {
-        if (charIndex.current < words[wordIndex.current].length) {
-            wordElement.current.textContent += words[wordIndex.current].charAt(charIndex.current);
-            charIndex.current++;
-            setTimeout(type, typingDelay);
-        } else {
-            setTimeout(erase, newTextDelay);
-        }
-    }, [typingDelay, newTextDelay, words, wordElement]);
-
     const erase = useCallback(() => {
         if (charIndex.current > 0) {
             wordElement.current.textContent = words[wordIndex.current].substring(0, charIndex.current - 1);
@@ -23,12 +13,23 @@ const useTypingEffect = (wordElement, words, typingDelay = 100, erasingDelay = 1
             wordIndex.current = (wordIndex.current + 1) % words.length;
             setTimeout(type, typingDelay + 1100);
         }
-    }, [erasingDelay, typingDelay, words, wordElement, type]);
+    }, [erasingDelay, typingDelay, words, wordElement]);
+
+    const type = useCallback(() => {
+        if (charIndex.current < words[wordIndex.current].length) {
+            wordElement.current.textContent += words[wordIndex.current].charAt(charIndex.current);
+            charIndex.current++;
+            setTimeout(type, typingDelay);
+        } else {
+            setTimeout(erase, newTextDelay);
+        }
+    }, [typingDelay, newTextDelay, words, wordElement, erase]);
 
     useEffect(() => {
         const timer = setTimeout(type, newTextDelay + 250);
         return () => clearTimeout(timer);
     }, [type, newTextDelay]);
+
 };
 
 export default useTypingEffect;
