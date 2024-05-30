5e21429b63f4ca01628a01555728a5cd1e60f4f8
Theo <thoecastillo@yahoo.com>
2024-05-30 17:07:10 +0200

    up

--- src/Composants/Home/useTypingEffect.js
+++ src/Composants/Home/useTypingEffect.js
@@ -4,31 +4,34 @@ const useTypingEffect = (wordElement, words, typingDelay = 100, erasingDelay = 1
     const wordIndex = useRef(0);
     const charIndex = useRef(0);
 
-    const erase = useCallback(() => {
-        if (charIndex.current > 0) {
-            wordElement.current.textContent = words[wordIndex.current].substring(0, charIndex.current - 1);
-            charIndex.current--;
-            setTimeout(erase, erasingDelay);
-        } else {
-            wordIndex.current = (wordIndex.current + 1) % words.length;
-            setTimeout(type, typingDelay + 1100);
-        }
-    }, [erasingDelay, typingDelay, words, wordElement]);
+    const type = useRef(null);
+    const erase = useRef(null);
 
-    const type = useCallback(() => {
+    type.current = useCallback(() => {
         if (charIndex.current < words[wordIndex.current].length) {
             wordElement.current.textContent += words[wordIndex.current].charAt(charIndex.current);
             charIndex.current++;
-            setTimeout(type, typingDelay);
+            setTimeout(type.current, typingDelay);
         } else {
-            setTimeout(erase, newTextDelay);
+            setTimeout(erase.current, newTextDelay);
         }
-    }, [typingDelay, newTextDelay, words, wordElement, erase]);
+    }, [typingDelay, newTextDelay, words, wordElement]);
+
+    erase.current = useCallback(() => {
+        if (charIndex.current > 0) {
+            wordElement.current.textContent = words[wordIndex.current].substring(0, charIndex.current - 1);
+            charIndex.current--;
+            setTimeout(erase.current, erasingDelay);
+        } else {
+            wordIndex.current = (wordIndex.current + 1) % words.length;
+            setTimeout(type.current, typingDelay + 1100);
+        }
+    }, [erasingDelay, typingDelay, words, wordElement]);
 
     useEffect(() => {
-        const timer = setTimeout(type, newTextDelay + 250);
+        const timer = setTimeout(type.current, newTextDelay + 250);
         return () => clearTimeout(timer);
-    }, [type, newTextDelay]);
+    }, [newTextDelay]);
 
 };
 
